95767e81c572c248b58f1dfab0a536f8aae4aa04
redgyuf <faramirex070@gmail.com>
2017-03-17 09:28:17 +0100

    added some game logics

--- client/Main.java
+++ client/Main.java
@@ -13,6 +13,14 @@ public class Main {
 		int port = sc.nextInt();
 		
 		Communicator cm = new Communicator(ip, port);
+		cm.waitForData();
+		
+		while(cm.getMsg().isGameRuns()){
+			System.out.println("Enter your number: ");
+			cm.getMsg().setNumberPlayer2(sc.nextInt());
+			cm.sendData();
+			cm.waitForData();
+		}
 		
 
 	}
