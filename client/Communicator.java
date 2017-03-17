1debb11889eb4f855c7c7ffdad55f9e8e8e40667
redgyuf <faramirex070@gmail.com>
2017-03-17 09:21:29 +0100

    communicator methods added

--- client/Communicator.java
+++ client/Communicator.java
@@ -9,6 +9,7 @@ public class Communicator {
 	private Socket sc;
 	private ObjectOutputStream os;
 	private ObjectInputStream is;
+	private Message msg;
 	
 	
 	public Communicator(String ip, int port){
@@ -17,6 +18,24 @@ public class Communicator {
 			os = new ObjectOutputStream(sc.getOutputStream());
 			is = new ObjectInputStream(sc.getInputStream());
 			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void waitForData(){
+		System.out.println("Waiting for server!");
+		try {
+			msg = (Message) is.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendData(){
+		System.out.println("Sending data to server!");
+		try {
+			os.writeObject(msg);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
