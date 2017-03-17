a0639039e2249dc1a6f62d738134b2a301434612
redgyuf <faramirex070@gmail.com>
2017-03-17 09:18:28 +0100

    added some console input

--- client/Main.java
+++ client/Main.java
@@ -1,9 +1,18 @@
 package client;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Game Started!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ip:");
+		String ip = sc.nextLine();
+		System.out.println("port:");
+		int port = sc.nextInt();
 		
+		Communicator cm = new Communicator(ip, port);
 		
 
 	}
